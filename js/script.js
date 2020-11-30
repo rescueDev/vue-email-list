b11363fd68a553b6faf1b7b01e05e8cbf16bcd6f
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-30 15:18:48 +0100

    basic style and generator on click button

--- js/script.js
+++ js/script.js
@@ -5,15 +5,32 @@ var app = new Vue({
   data: {
     emails: [],
   },
-  mounted: function () {
-    for (let i = 0; i < 10; i++) {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((risposta) => {
-          console.log(risposta);
-          console.log(risposta.data);
-          this.emails.push(risposta.data.response);
-        });
-    }
+  //   mounted: function () {
+  //     for (let i = 0; i < 10; i++) {
+  //       axios
+  //         .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  //         .then((risposta) => {
+  //           //   console.log(risposta);
+  //           //   console.log(risposta.data);
+  //           this.email = risposta.data.response;
+  //           this.emails.push(this.email);
+  //         });
+  //     }
+  //   },
+  methods: {
+    generaList() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((risposta) => {
+            //   console.log(risposta);
+            //   console.log(risposta.data);
+            this.email = risposta.data.response;
+            this.emails.push(this.email);
+          });
+      }
+      //resetto array per averne sempre 10 ad ogni click del button
+      this.emails = [];
+    },
   },
 });
